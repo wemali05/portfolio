91486771ddd4b84a91a0afc21fb0552f6a88d8d8
wemali05 <alfredwemali05@gmail.com>
2019-11-07 08:54:53 +0300

    BlogCollection

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostCollection;
+
+class PostController extends Controller
+{
+    public function store(Request $request)
+    {
+        $post = new Post([
+        'title' => $request->get('title'),
+        'body' => $request->get('body')
+      ]);
+
+        $post->save();
+
+        return response()->json('successfully added');
+    }
+
+    public function index()
+    {
+        return new PostCollection(Post::all());
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return response()->json($post);
+    }
+
+    public function update($id, Request $request)
+    {
+        $post = Post::find($id);
+
+        $post->update($request->all());
+
+        return response()->json('successfully updated');
+    }
+    public function show()
+    {
+    }
+
+    public function delete($id)
+    {
+        $post = Post::find($id);
+
+        $post->delete();
+
+        return response()->json('successfully deleted');
+    }
+}
